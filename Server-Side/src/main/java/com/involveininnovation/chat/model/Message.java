db0aa847b133095864fe421b4ee0a3fca8e2742b
AakashDevilstar <akashraturi2001@gmail.com>
2024-02-02 11:38:10 +0530

    Backend Code

---
+++ Server-Side/src/main/java/com/involveininnovation/chat/model/Message.java
@@ -0,0 +1,49 @@
+package com.involveininnovation.chat.model;
+
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Message {
+    private String senderName;
+    public String receiverName;
+    public String message;
+    private String date;
+    private Status status;
+	public String getSenderName() {
+		return senderName;
+	}
+	public void setSenderName(String senderName) {
+		this.senderName = senderName;
+	}
+	public String getReceiverName() {
+		return receiverName;
+	}
+	public void setReceiverName(String receiverName) {
+		this.receiverName = receiverName;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+    
+}
